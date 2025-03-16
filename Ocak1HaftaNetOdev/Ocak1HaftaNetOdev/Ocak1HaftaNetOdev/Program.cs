8f581728fc096d8fb76511b662b5a697aaa14950
Veysel Karani Saydam <vsaydam23@gmail.com>
2025-03-16 16:42:41 +0300

    Ocak 1. Hafta Ödevi Tamamlandı

---
+++ Ocak1HaftaNetOdev/Ocak1HaftaNetOdev/Ocak1HaftaNetOdev/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Adınızı girin: ");
+        string ad = Console.ReadLine();
+        Console.WriteLine($"Merhaba, {ad}!");
+    }
+}
